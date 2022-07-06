cf0f0ffc1b60989afbbe5494cbf87eb97e2a2df9
vyerena <106994479+vyerena@users.noreply.github.com>
2022-07-06 09:42:06 -0500

    intsorter.py

--- intsorter.py
+++ intsorter.py
@@ -1,6 +1,16 @@
-numbers = list(map(int, input().split(" ")))
-print(numbers)
-prod = 1
-for i in numbers:
-    prod *= i
-print(prod)
+n = int(input("List length = "))
+list1 = []
+
+for i in range(n):
+    x = int(input("Elements  (0 a 9) = "))
+    if (i == 0) or (x > list1[- 1]):
+        list1.append(x)
+    else:
+        pos = 0
+        while pos < len(list1):
+            if x <= list1[pos]:
+                list1.insert(pos, x)
+                break
+            pos = pos + 1
+
+print(list1)
